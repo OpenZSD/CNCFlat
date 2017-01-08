b9c36ca5dd4bd325146155bba240e5b413ea9298
OpenZSD <open@z-softdevelopment.com>
2017-01-07 23:29:54 -0800

    Progress Commit;
    More refactoring of QA code. Tests can now be part of the production
    code, but not compiled into release binary. This is still a work in
    progress. Next step is to factor in UIUtil's findAndAssert call to use
    QA code (as to not terminate unit tests upon failure).

--- Build/CNCFlat/QAUtil.h
+++ Build/CNCFlat/QAUtil.h
@@ -1,3 +1,13 @@
+/*! \file QAUtil.h
+    \brief Macros and functions for general QA and Unit Tests
+
+    Macros are designed to allow QA code to be reduced or removed when compiling to release.
+    QA Macros are composed of the following:
+    -reporter: Unit test GUI(or non-gui) implementing IQAReporter reponsible for the execution and reporting of tests
+    -tester: Represents a shared pointer singleton implementing IQATester responsible for a suite of tests
+    -test: A shared porter implementing IQATest representing a single test that reports to tester
+*/
+
 #ifndef QAUTIL_H
 #define QAUTIL_H
 
@@ -12,6 +22,7 @@ public:
     static void regTestReporter();
     static void voidTestReporter();
 
+    static void _reportTest(bool pass, QString msg, int lvl, QString srcPt);
     static void _termWithMsg(QString msg);
     static void _promptWithMsg(QString msg);
     static void _stderr(QString msg);
@@ -24,37 +35,43 @@ private:
     static QTextStream sStdOut;
 };
 
-//removes UI code in the event of QA automation
-#ifdef AQ_AUTOMATED
+/**<GENERAL DEVELOPMENT QA>**/
+#ifdef QA_AUTOMATED
+/*! \def AUTO_CHECK(a)
+    \brief Mutes statement \a a durring automated unit tests. This is to avoid dialog messages.*/
 #define AUTO_CHECK(a) {}
 
 #else
 #define AUTO_CHECK(a) a
 #endif
 
-
 #ifndef QT_DEBUG //production mode
 #define QA_STAT(msg) {} //no stats are reported in production
-#define QA_TEST(check, msg) {} //no unit tests in production
 #define QA_WARN(check, msg)\
 {\
-    if(!check)\
+    if(!(check))\
     {\
         QAUtil::_stderr(msg);\
     }\
-}\
+}
+#define QA_TEST(pass, msg, lvl, srcPt) QA_WARN(pass,msg) //becomes another warning in production
 
-#else
-#define ASRT_EQ(a, b, msg) {}
-#define ASRT_TRUE(a, msg) {}
+#else //development mode
+/*! \def QA_STAT(msg)
+    \brief Prints \a msg to stdout for development logging.
+
+    NOTE: this will be muted in release mode.*/
 #define QA_STAT(msg)\
 {\
-    ostream << msg;\
+    QAUtil::_stdout(msg);\
 }
-#define QA_TEST(check, msg) {}
+/*! \def QA_WARN(check, msg)
+    \brief An assert that prints \a msg to stderr upon \a check returning false.
+
+    NOTE: this will also produce a dialog message in devopment mode.*/
 #define QA_WARN(check, msg)\
 {\
-    if(!check)\
+    if(!(check))\
     {\
         QAUtil::_stderr(msg);\
         AUTO_CHECK(QAUtil::_promptWithMsg(msg);)\
@@ -67,10 +84,35 @@ private:
 {\
     if(!f)\
     {\
+        QAUtil::_stderr(msg);\
         QAUtil::_termOnFail(m);\
     }\
 }
 
 
+/**<UNIT TESTING QA>**/
+//removes UI code in the event of QA Test automation
+#ifdef QA_AUTOMATED
+#define TEST_SUITE(reporter, tester) true //in progress, will replace
+#define TEST_STAGE(tester, test) true //in progress, will replace
+
+
+#else
+#define TEST_SUITE(reporter, test) true //defaults to true, no tester available
+#define TEST_STAGE(tester, test) true //defaults to true, no tester available
+#endif
+
+#ifndef QT_DEBUG //production mode
+#define TEST_STAGE_ASRT(tester, check, msg, asrtTest) QA_WARN(check, msg)
+
+#else
+#define TEST_STAGE_ASRT(tester, check, msg, asrtTest)\
+{\
+    asrtTest->setMessage(msg); \
+    asrtTest->assert(check); \
+    tester->report(asrtTest); \
+}
+
+#endif
 
 #endif // QAUTIL_H
